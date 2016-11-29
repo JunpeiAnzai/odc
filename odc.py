089979565ca0ca708be7fcf37c2ff0c9671cb92c
Jumpei Anzai <junpei.anzai@gmail.com>
2016-11-29 18:40:13 +0900

    Add watchdog

--- odc.py
+++ odc.py
@@ -4,6 +4,12 @@ import onedrivesdk
 from onedrivesdk.helpers import GetAuthCodeServer
 import os
 
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
 input = getattr(__builtins__, 'raw_input', input)
 
 
@@ -21,3 +27,19 @@ def main():
     code = GetAuthCodeServer.get_auth_code(auth_url, redirect_uri)
 
     client.auth_provider.authenticate(code, redirect_uri, client_secret)
+
+    # watchdog
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
